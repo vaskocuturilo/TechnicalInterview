163180a54e61d81af041e948ed5f751a1a23d5a9
asm <asm@dostavista.ru>
2023-01-26 16:13:28 +0100

    DV-00005: Add Errors Page.

--- src/main/java/com/example/demo/controller/TechnicalInterviewController.java
+++ src/main/java/com/example/demo/controller/TechnicalInterviewController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -34,7 +32,7 @@ public class TechnicalInterviewController {
         TechnicalInterviewEntity randomTechnicalInterviewQuestion = technicalInterviewService.getRandomQuestion();
         model.addAttribute("randomTechnicalInterviewQuestion", randomTechnicalInterviewQuestion);
 
-        return "redirect:/";
+        return "errors";
     }
 
 
@@ -60,8 +58,8 @@ public class TechnicalInterviewController {
     }
 
     @PostMapping("/upload")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("message", "Sadly, this functionality doesn't work, now.");
-        return "redirect:/";
+    public String uploadTechnicalInterviewTasks() {
+
+        return "errors";
     }
 }
