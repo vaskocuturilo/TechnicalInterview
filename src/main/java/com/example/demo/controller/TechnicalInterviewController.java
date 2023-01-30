eb937d50b7f0c87645addc3bb13dc8ae672b68c9
asm <vaskocuturilo@rambler.ru>
2023-01-30 11:55:49 +0100

    DV-000015: Add upload functionality.

--- src/main/java/com/example/demo/controller/TechnicalInterviewController.java
+++ src/main/java/com/example/demo/controller/TechnicalInterviewController.java
@@ -1,27 +1,36 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.TechnicalInterviewEntity;
+import com.example.demo.exception.StorageFileNotFoundException;
+import com.example.demo.service.StorageService;
 import com.example.demo.service.TechnicalInterviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/api/v1")
 public class TechnicalInterviewController {
-    final TechnicalInterviewService technicalInterviewService;
+    @Autowired
+    private final TechnicalInterviewService technicalInterviewService;
+
+    private final StorageService storageService;
 
     @Autowired
-    public TechnicalInterviewController(TechnicalInterviewService technicalInterviewService) {
+    public TechnicalInterviewController(TechnicalInterviewService technicalInterviewService, StorageService storageService) {
         this.technicalInterviewService = technicalInterviewService;
+        this.storageService = storageService;
     }
 
     @GetMapping("")
     public String viewHomePage() {
-        return "index";
+        return "index-1";
     }
 
     @GetMapping("/questions")
@@ -63,14 +72,23 @@ public class TechnicalInterviewController {
     }
 
     @PostMapping("/upload")
-    public String uploadTechnicalInterviewTasks() {
+    public String handleFileUpload(@RequestParam("upload") MultipartFile upload,
+                                   RedirectAttributes redirectAttributes) {
 
-        return "errors";
+        storageService.store(upload);
+        redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + upload.getOriginalFilename() + "!");
+
+        return "redirect:/api/v1/questions";
     }
 
     @RequestMapping("/reset")
     public String resetAllCompletedTechnicalInterviewTasks() {
         technicalInterviewService.resetAllCompletedTechnicalInterviewTasks();
         return "redirect:/api/v1/questions";
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
     }
 }
