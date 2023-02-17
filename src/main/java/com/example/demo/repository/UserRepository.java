3bb6491a10d17d64e3ba2aa399dc9908c4734976
asm <vaskocuturilo@rambler.ru>
2023-02-17 11:04:20 +0100

    DV-000031: Add functionality for roles.

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -4,9 +4,7 @@ import com.example.demo.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByEmail(final String email);
 }
