f59fa1102dff7766ddb1ec36313ab53c6d48dd24
asm <vaskocuturilo@rambler.ru>
2023-02-21 10:20:15 +0100

    DV-000035: Add unathorized exception.

--- src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -3,20 +3,22 @@ package com.example.demo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
     private final DataSource dataSource;
 
     public WebSecurityConfig(DataSource dataSource) {
@@ -49,10 +51,12 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .anonymous().disable()
                 .csrf().disable()
+                .headers((headers) -> headers
+                        .frameOptions().disable())
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/").permitAll()
-                        .requestMatchers("/css/**").permitAll()
+                        .requestMatchers("/", "/css/*", "/js/*").permitAll()
                         .requestMatchers("/api/v1/admin/users").hasAnyAuthority("ADMIN")
                         .requestMatchers("/api/v1/create").hasAnyAuthority("GUEST", "USER", "ADMIN")
                         .requestMatchers(HttpMethod.GET, "/api/v1/register").permitAll()
