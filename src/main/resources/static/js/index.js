6077728967d582f3f98afdb0b5aa83b5d8ee8a4e
asm <vaskocuturilo@rambler.ru>
2023-02-25 10:59:28 +0100

    FIX: added menu on all pages.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const close = document.querySelector('.close-modal');
+const openModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+document.addEventListener('keydown', (event) => {
+    const key = event.key;
+    if (key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+})
