d31e35df0379045e958d035981100a754fe4fbda
asm <vaskocuturilo@rambler.ru>
2023-02-27 18:00:10 +0100

    Fix: fixed ui automation scripts.

--- src/test/java/com/example/demo/SimpleUITest.java
+++ src/test/java/com/example/demo/SimpleUITest.java
@@ -21,10 +21,10 @@ public class SimpleUITest {
     private static WebDriver webDriver;
     private WebDriverWait webDriverWait;
     private static final int DELAY = 10;
-    private static By TASK_NAME = By.xpath("//input[@name='taskName']");
-    private static By DESCRIPTION = By.xpath("//input[@name='description']");
+    private static By TITLE = By.xpath("//input[@id='title_name_input']");
+    private static By DESCRIPTION = By.xpath("//input[@id='description_input']");
     private static By ADD_QUESTION_BUTTON = By.xpath("//input[@id='button_add_question']");
-    private static By DELETE_LINK = By.xpath("//a[@id='cell_delete']");
+    private static By DELETE_LINK = By.xpath("//table[@class='table table-striped table-bordered']//td[contains(text(),'Question for delete')]/following-sibling::td//button[@id='cell_delete']");
     private static By DELETE_BUTTON = By.xpath("//input[@id='delete_button']");
     private static By RESET_ALL_BUTTON = By.xpath("//input[@id='reset_button']");
     private static By TABLE = By.xpath("//table[@id='main_table']");
@@ -37,9 +37,9 @@ public class SimpleUITest {
     private static By FAIL_COUNT = By.xpath("//h1[@id='fail_count']");
     private static By UPLOAD_FILE = By.xpath("//input[@id='upload_file']");
     private static By UPLOAD_FILE_BUTTON = By.xpath("//input[@id='upload_questions']");
-    private static By COMPLETE_CELL = By.xpath("//table[@class='table table-striped table-bordered']//td[contains(text(),'Test complete')]/following-sibling::td//a[@id='cell_complete']");
+    private static By COMPLETE_CELL = By.xpath("//table[@class='table table-striped table-bordered']//td[contains(text(),'Test complete')]/following-sibling::td//button[@id='cell_complete']");
     private static By TABLE_LIST = By.xpath("//table[@id='main_table']//tbody/tr");
-    private static By MESSAGE = By.xpath("//p[@id='info_message']");
+    private static By MESSAGE = By.xpath("//div[@id='info_message']");
     private static final String UPLOAD_MESSAGE = "You successfully uploaded file is: %s";
     private static final String ERROR_UPLOAD_MESSAGE = "Maximum upload size of %s exceeded";
 
@@ -63,8 +63,8 @@ public class SimpleUITest {
         webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(DELAY));
         webDriver.get("http://localhost:8080/");
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(MAIN_PAGE_LINK_QUESTIONS)).click();
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(USER_NAME)).sendKeys("admin");
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(PASSWORD)).sendKeys("admin");
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(USER_NAME)).sendKeys("admin@qa.team");
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(PASSWORD)).sendKeys("123456");
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(SIGN_IN)).click();
     }
 
@@ -81,22 +81,23 @@ public class SimpleUITest {
 
         Integer failCount = Integer.parseInt(webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(FAIL_COUNT)).getText());
 
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(TASK_NAME)).sendKeys("Test1");
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(TITLE)).sendKeys("Test1");
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(DESCRIPTION)).sendKeys("Test1");
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(ADD_QUESTION_BUTTON)).click();
 
         assertThat(Integer.parseInt(webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(TOTAL_COUNT)).getText())).isEqualTo(++totalCount);
         assertThat(Integer.parseInt(webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(FAIL_COUNT)).getText())).isEqualTo(++failCount);
+
     }
 
     @Test
     public void testVerifyCompleteQuestion() {
-        final String taskName = "Test complete";
+        final String title = "Test complete";
         webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(DELAY));
         Integer count = Integer.parseInt(webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(PASS_COUNT)).getText());
 
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(TASK_NAME)).sendKeys(taskName);
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(DESCRIPTION)).sendKeys(taskName);
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(TITLE)).sendKeys(title);
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(DESCRIPTION)).sendKeys(title);
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(ADD_QUESTION_BUTTON)).click();
 
         scrollPage();
@@ -108,7 +109,7 @@ public class SimpleUITest {
     @Test
     public void testVerifyTable() {
         webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(DELAY));
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(TASK_NAME)).sendKeys("Test1");
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(TITLE)).sendKeys("Test1");
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(DESCRIPTION)).sendKeys("Test1");
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(ADD_QUESTION_BUTTON)).click();
 
@@ -117,17 +118,20 @@ public class SimpleUITest {
 
     @Test
     public void testDeleteElementFromTable() {
+        final String title = "Question for delete";
         webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(DELAY));
         assertThat(webDriver.getTitle()).isEqualTo("Technical interview questions");
 
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(TASK_NAME)).sendKeys("Test1");
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(DESCRIPTION)).sendKeys("Test1");
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(TITLE)).sendKeys(title);
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(DESCRIPTION)).sendKeys(title);
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(ADD_QUESTION_BUTTON)).click();
 
         assertThat(webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(TABLE)).isDisplayed());
 
         Integer count = Integer.parseInt(webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(TOTAL_COUNT)).getText());
 
+        scrollPage();
+        waiter(3000);
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(DELETE_LINK)).click();
         Alert alert = webDriver.switchTo().alert();
         alert.accept();
@@ -154,7 +158,7 @@ public class SimpleUITest {
     }
 
     @Test
-    public void testUploadCorrectSizeFile() {
+    public void testUploadCorrectJsonSizeFile() {
         File file = new File("src/main/resources/upload.json");
         webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(DELAY));
         assertThat(webDriver.getTitle()).isEqualTo("Technical interview questions");
@@ -167,7 +171,33 @@ public class SimpleUITest {
     }
 
     @Test
-    public void testUploadInCorrectSizeFile() {
+    public void testUploadCorrectCsvSizeFile() {
+        File file = new File("src/main/resources/upload.csv");
+        webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(DELAY));
+        assertThat(webDriver.getTitle()).isEqualTo("Technical interview questions");
+
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(UPLOAD_FILE)).sendKeys(file.getAbsolutePath());
+
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(UPLOAD_FILE_BUTTON)).click();
+
+        assertThat(webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(MESSAGE)).getText()).isEqualTo(String.format(UPLOAD_MESSAGE, "upload.csv"));
+    }
+
+    @Test
+    public void testUploadCorrectXlsxSizeFile() {
+        File file = new File("src/main/resources/upload.xlsx");
+        webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(DELAY));
+        assertThat(webDriver.getTitle()).isEqualTo("Technical interview questions");
+
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(UPLOAD_FILE)).sendKeys(file.getAbsolutePath());
+
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(UPLOAD_FILE_BUTTON)).click();
+
+        assertThat(webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(MESSAGE)).getText()).isEqualTo(String.format(UPLOAD_MESSAGE, "upload.xlsx"));
+    }
+
+    @Test
+    public void testUploadInCorrectJsonSizeFile() {
         File file = new File("src/main/resources/10mb-sample.json");
         webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(DELAY));
         assertThat(webDriver.getTitle()).isEqualTo("Technical interview questions");
