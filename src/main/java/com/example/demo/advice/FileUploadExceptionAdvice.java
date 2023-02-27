c4a5c43c1f862f7531fe085e41dccdad4eccb08a
asm <vaskocuturilo@rambler.ru>
2023-02-27 10:59:22 +0100

    Dv-000041: Add functionality for upload excel file.

--- src/main/java/com/example/demo/advice/FileUploadExceptionAdvice.java
+++ src/main/java/com/example/demo/advice/FileUploadExceptionAdvice.java
@@ -1,6 +1,7 @@
 package com.example.demo.advice;
 
 
+import com.example.demo.exception.NotCorrectlyCellException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,11 +14,21 @@ public class FileUploadExceptionAdvice {
     @Value("${spring.http.multipart.max-file-size}")
     String maxSizeInMB;
 
+    @Value("${cell.info.message}")
+    private String infoMessage;
+
     @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public String handleError2(MaxUploadSizeExceededException ex, RedirectAttributes redirectAttributes) {
+    public String handleError2(RedirectAttributes redirectAttributes) {
 
         redirectAttributes.addFlashAttribute("message", "Maximum upload size of " + maxSizeInMB + " exceeded");
 
+        return "redirect:/api/v1/questions";
+    }
+
+    @ExceptionHandler(value = NotCorrectlyCellException.class)
+    public String exception(RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("message", " " + infoMessage + " ");
         return "redirect:/api/v1/questions";
     }
 }
--- src/main/java/com/example/demo/advice/UserExceptionController.java
+++ src/main/java/com/example/demo/advice/UserExceptionController.java
@@ -1,6 +1,7 @@
 package com.example.demo.advice;
 
 import com.example.demo.exception.UserFoundException;
+import com.example.demo.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,5 +12,10 @@ public class UserExceptionController {
     @ExceptionHandler(value = UserFoundException.class)
     public ResponseEntity<Object> exception(UserFoundException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.FOUND);
+    }
+
+    @ExceptionHandler(value = UserNotFoundException.class)
+    public ResponseEntity<Object> exception(UserNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
